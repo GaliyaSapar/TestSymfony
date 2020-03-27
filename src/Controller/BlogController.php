bd2d22e55fd3c2afaeff91c69afe774e3f6ca0bd
Galiya Sapar <s.galiya.m@gmail.com>
2020-03-27 18:55:22 +0600

    test

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/{page}", name="blog_list", methods={"GET", "HEAD"} requirements={"page"="\d+"})
      *
      *
-     * * @Route("/blog/{page<\d+>}", name="blog_list")
+     *
+     *
      *
      *
      * * @Route("/blog/{page<\d+>?1}", name="blog_list")
@@ -26,7 +26,7 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{slug}", name="blog_show")
      */
-    public function show(BlogPost $post)
+    public function show($post)
     {
         return $this->render('lucky/number.html.twig', [
             'number' => $post,
